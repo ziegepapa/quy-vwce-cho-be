f91d573afb99b9702ca8630dab14a40936cc1a3e
ziegepapa <phamchien0891@gmail.com>
2026-07-29 21:56:09 +0200

    feat: bọc AuthProvider quanh app

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import { AuthProvider } from "./lib/auth";
 import App from "./App";
 import "./index.css";
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode><HashRouter><App /></HashRouter></StrictMode>
+  <StrictMode>
+    <HashRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HashRouter>
+  </StrictMode>,
 );
