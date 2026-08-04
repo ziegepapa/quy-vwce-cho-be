47603109c7d88cd498d560478e607b32779e3ebc
ziegepapa <phamchien0891@gmail.com>
2026-08-04 15:57:26 +0200

    fix(quotes): point backup facade to real v3 modules

--- src/lib/db.backup.ts
+++ src/lib/db.backup.ts
@@ -1,9 +1,9 @@
-export { exportBackup } from "./db.export";
+export { exportBackup } from "./db.m08";
 export {
   importBackup,
   clearAllData,
   clearUserBusinessData,
   getOrCreateChecklist,
   countLocalData,
-} from "./db.import";
+} from "./db.m09";
 export { uid } from "./defaults";
