a90de3e659d85382725ec81846ec7b46ad0549cd
ziegepapa <phamchien0891@gmail.com>
2026-08-14 04:50:16 +0200

    fix(app): fail-closed khi promise ứng dụng bị lỗi (#133)
    Thêm AppFailureBoundary ở cấp root để chặn unhandled promise rejection (trừ AbortError), hiển thị trạng thái fail-closed và cho phép retry remount. Kèm regression tests.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./lib/auth";
 import App from "./App";
+import AppFailureBoundary from "./components/AppFailureBoundary";
 import "./index.css";
 import "./styles/dock.css";
 import "./styles/nav.css";
@@ -20,10 +21,12 @@ import "./styles/dark-screen-finish.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </HashRouter>
+    <AppFailureBoundary>
+      <HashRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </HashRouter>
+    </AppFailureBoundary>
   </StrictMode>,
 );
